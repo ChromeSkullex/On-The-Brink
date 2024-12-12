f885eec72cc41aa34c970c37ada4f3a70841b4ab
Sazhelle <szlgmoulton@gmail.com>
2024-12-11 22:44:37 -0500

    Changed on-the-brink to onthebrink
    Keep it consistent throughout codebase, and it's easier

--- fabric/src/main/java/com/onthebrink/OnTheBrink.java
+++ fabric/src/main/java/com/onthebrink/OnTheBrink.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OnTheBrink implements ModInitializer {
-	public static final String MOD_ID = "on-the-brink";
+	public static final String MOD_ID = "onthebrink";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
